d04cb68b8196f86339d37434547ec4bf2f6f6bf2
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-01 14:48:58 +0700

    Add LeftSidebar to HomeLayout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+
+import LeftSidebar from '@/components/ui/shared/LeftSidebar'
+
 export default function HomeLayout({
 	children,
 }: Readonly<{
@@ -6,8 +10,21 @@ export default function HomeLayout({
 	return (
 		<div className='relative flex flex-col'>
 			<main className='relative flex bg-black-3'>
-				LeftSidebar
-				{children}
+				<LeftSidebar />
+				<section className='flex min-h-screen flex-1 flex-col px-4 sm:px-14'>
+					<div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
+						<div className='flex h-16 items-center justify-between md:hidden'>
+							<Image
+								src='/icons/logo.svg'
+								width={30}
+								height={30}
+								alt='menu icon'
+							/>
+							MobileNav
+						</div>
+						<div className='flex flex-col md:pb-14'>{children}</div>
+					</div>
+				</section>
 				RightSidebar
 			</main>
 		</div>
